0fe2df0ddda5b541c52ba5c4db3835f06e6acaa5
JamesHow1ett <mopis101@gmail.com>
2020-01-17 15:21:43 +0200

    блоки как ссылки + ховер еффект

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const anchors = document.querySelectorAll('a[href*="#"]');
 const goToTopButton = document.getElementById('goToTop');
 const navBar = document.getElementById('nav-bar');
 const iconBar = document.getElementById('icon-bar');
+const aboutCard = document.querySelectorAll('div.about-card');
 
 for (let anchor of anchors) {
   anchor.addEventListener("click", event => {
@@ -25,7 +26,7 @@ const scrollFunction = () => {
   } else {
     goToTopButton.style.display = "none";
   }
-}
+};
 
 const goToTop = () => {
   document.body.scrollIntoView({
@@ -36,7 +37,7 @@ const goToTop = () => {
     behavior: "smooth",
     block: "start"
   });
-}
+};
 
 const addStickyClass = () => {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
@@ -45,9 +46,37 @@ const addStickyClass = () => {
     iconBar.classList.remove("icon-bar-sticky");
     iconBar.style.display = "none";
   }
-}
+};
 
 const phoneMask = IMask(
   document.getElementById('phone-mask'), {
     mask: '+{38}(000)000-00-00'
   });
+
+
+const aboutCardLikeLink = () => {
+  for (let i = 0; i < aboutCard.length; i++) {
+    aboutCard[i].onmouseout = () => {
+      aboutCard[i].style.background = '';
+    }
+    aboutCard[i].onmouseover = () => {
+      aboutCard[i].style.cursor = 'pointer';
+      aboutCard[i].style.background = 'rgba(0, 120, 201, 0.7)';
+    }
+  }
+}
+
+document.getElementById('online').addEventListener('click', () => {
+  location.href = 'https://online.hneu.edu.ua/'
+}, false);
+document.getElementById('language').addEventListener('click', () => {
+  location.href = 'https://docs.google.com/forms/d/1E0dxJgdGLIkMb6GANqCHI_HMMTH1iDu_nO_fNkPUExk/viewform?ts=5dcdd5f1&edit_requested=true'
+}, false);
+document.getElementById('robot').addEventListener('click', () => {
+  location.href = 'https://docs.google.com/forms/d/e/1FAIpQLScHMsJxJEp7mTqbay10gtTVnFS2EKQ7bcLG9HM_Pyvk3yiApw/viewform'
+}, false);
+document.getElementById('zno').addEventListener('click', () => {
+  location.href = 'https://docs.google.com/forms/d/e/1FAIpQLScuDl9k0k8HEAsrJLCH0PloVrfqopcmG5G-SDh37p_QsBSPig/viewform'
+}, false);
+
+aboutCardLikeLink();
