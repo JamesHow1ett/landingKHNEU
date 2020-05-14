9c42f3985db720a3968f54ede8ad455512eec07e
JamesHow1ett <mopis101@gmail.com>
2020-05-14 12:39:06 +0300

    добавил валидацию формы

--- js/modules/validate.js
+++ js/modules/validate.js
@@ -1,6 +1,18 @@
 const nameRegExp = new RegExp("^[a-zа-яА-ЯёЁЇїІіЄєҐґ-]+$");
 const emailRegExp = new RegExp("^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$");
 
+const isValid = (value, elem, type) => {
+  const res = type.exec(value);
+  console.log(elem);
+  if (res) {
+    elem.classList.remove('form__input_invalid');
+    elem.classList.add('form__input_valid');
+  } else {
+    elem.classList.remove('form__input_valid');
+    elem.classList.add('form__input_invalid');
+  }
+}
+
 export const validateForm = (event) => {
   const target = event.target;
   const key = target.getAttribute('id');
@@ -14,17 +26,5 @@ export const validateForm = (event) => {
     case 'email':
       isValid((target.value), target, emailRegExp);
       break;
-  }
-}
-
-const isValid = (value, elem, type) => {
-  const res = type.exec(value);
-  console.log(elem);
-  if (res) {
-    elem.classList.remove('form__input_invalid');
-    elem.classList.add('form__input_valid');
-  } else {
-    elem.classList.remove('form__input_valid');
-    elem.classList.add('form__input_invalid');
   }
 }
